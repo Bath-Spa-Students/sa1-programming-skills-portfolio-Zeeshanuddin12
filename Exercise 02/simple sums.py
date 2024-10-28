55970993d9dcff0a2af30fe79ede8460ac553bf9
Zeeshanuddin12 <mohammadzeeshanuddin128@gmail.com>
2024-10-28 09:52:38 +0400

    simple sums

---
+++ Exercise 02/simple sums.py
@@ -0,0 +1,12 @@
+#The 1st step is to declare a variable and set its initial value to the integer 13.
+first_number = 13
+#The 2nd step is to claim a second variable and set its initial value to the integer 22.
+second_number = 22
+# The 3rd step is to declare a third variable and set its initial value to 37.
+third_number = 37
+# Step 4: Declare the integer number 53 as the initial value of the fourth variable.
+forth_number = 53
+# Step 5: Add the four integers together and put the result in a new variable.
+sum_of_numbers = first_number + second_number + third_number + forth_number
+# Step 6: The sum should be shown on the console.
+print("The sum of", first_number, second_number, third_number, "and", forth_number, "is:", sum_of_numbers)
