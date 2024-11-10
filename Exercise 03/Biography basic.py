f72c57997374b08a8fc9c69d0e28379de6bff52d
Zeeshanuddin12 <mohammadzeeshanuddin128@gmail.com>
2024-11-10 22:35:16 +0400

    Biography basic

---
+++ Exercise 03/Biography basic.py
@@ -0,0 +1,5 @@
+#This is the code for inputing information in a dictionary
+my_information= {"name":"Zeeshan","hometown":"Jamshedpur","age": 22}
+
+#This is a code for priningt the dictionary.
+print(f"NAME: {my_information['name']}\nHOMETOWN: {my_information['hometown']}\nAGE: {my_information['age']}")
