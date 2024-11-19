7be88900ff94c6cdcf543257ee27e48c2c57b307
Zeeshanuddin12 <mohammadzeeshanuddin128@gmail.com>
2024-11-19 14:34:08 +0400

    control_structures_alien

---
+++ Bonus Code lab/control_structures_alien.py
@@ -0,0 +1,10 @@
+# Define the alien color
+alien_color = "green"
+
+# Version that passes
+if alien_color == "green":
+    print("Player earned 5 points!")
+
+# Version that fails (no output)
+if alien_color == "blue":
+    print("Player earned 5 points!")
